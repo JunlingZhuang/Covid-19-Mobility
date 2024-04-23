797f54137d2fd9adfc80224745a33d2e378597a8
ZHUANG JUNLING <108850976+JunlingZhuang@users.noreply.github.com>
2024-04-23 17:19:01 -0400

    update state chart

--- components/dataViz.js
+++ components/dataViz.js
@@ -1,14 +1,15 @@
 import { loadData } from "./dataLoader.js";
 import { drawlineChart } from "./lineChart.js";
+import { drawStateChart } from "./stateChart.js";
 
 document.addEventListener("DOMContentLoaded", function () {
   const svgLineChart = d3.select(".us-graph").append("svg");
-  //   const svgNational = d3.select(".national-chart").append("svg");
+  const svgStateChart = d3.select(".lag").append("svg");
   //   const svgScatter = d3.select(".scatter-plot").append("svg");
 
   loadData().then((data) => {
     drawlineChart(svgLineChart, data.us_data);
-    // drawNationalChart(svgNational, data[1]);
+    drawStateChart(svgStateChart, data.state_data);
     // drawScatterPlot(svgScatter, data[2]);
   });
 });
---
+++ components/stateChart.js
@@ -0,0 +1,11 @@
+import config from "./config.js";
+
+
+function setup(svg_state, state_data) {
+    console.log("state data",state_data);
+}
+
+
+export function drawStateChart(svg_state, state_data) {
+    setup(svg_state, state_data)
+}
